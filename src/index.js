0191e8fcb772e157550a5a401838444cb8b84cb5
Roshni Kutty <roshni.ajkutty@gmail.com>
2017-07-07 14:12:31 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import InterestCalculator from './components/interest-calculator';
+import './index.css';
+import {Provider} from 'react-redux';
+import store from'./store';
+
+ReactDOM.render(
+    <Provider store={store}><InterestCalculator /></Provider>,
+    document.getElementById('root')
+);
